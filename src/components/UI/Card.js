5e902c64a5f7252d5c9b3cf38e224b281909b6b1
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-30 20:08:13 -0500

    adds simple styling

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-import './Card.css';
+import classes from './Card.module.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <></>
+        <>
+        <div className={`${classes.card} ${props.className}`}>
+            {props.children}
+        </div>
+        </>
     )
 }
 
