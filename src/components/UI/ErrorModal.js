cc7bc0046dd1b6bd3f057d597f62d2dfb927dd0d
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-30 21:43:46 -0500

    adds error modal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
+import classes from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+    return (
+	<>
+        <div className={classes.backdrop}>
+            <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button>Okie Dokie</Button>
+                </footer>
+            </Card>
+        </div>
+    </>
+    )
+}
+
+export default ErrorModal;
