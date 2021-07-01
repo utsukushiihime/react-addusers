d6930424282bc41d4aa5e3ed659cafb184b8c355
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-30 20:53:19 -0500

    adds base validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,7 +10,15 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        console.log(inputUsername, inputAge)
+        if(inputUsername.trim().length === 0 || inputAge.trim().length === 0 ) {
+            return;
+        }
+        if(+inputAge < 1) {
+            return;
+        }
+        console.log(inputUsername, inputAge);
+        setInputUsername('');
+        setInputAge('');
     };
 
     const usernameChangeHandler = (event) => {
@@ -24,9 +32,9 @@ const AddUser = (props) => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler} />
+                <input id="username" type="text" value={inputUsername} onChange={usernameChangeHandler} />
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
+                <input id="age" type="number" value={inputAge} onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
