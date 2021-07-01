18aa0c50778e40c19d757500567d6f6158ab4731
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-30 20:43:02 -0500

    adds handlers

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import './AddUser.css';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
 
-const AddUser = () => {
+const AddUser = (props) => {
+    const [inputUsername, setInputUsername] = useState('');
+    const [inputAge, setInputAge] = useState('');
+
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        console.log(inputUsername, inputAge)
+    };
+
+    const usernameChangeHandler = (event) => {
+        setInputUsername(event.target.value);
+    };
+    const ageChangeHandler = (event) => {
+        setInputAge(event.target.value);
+    };
+
     return (
-	<></>
-    )
-}
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" onChange={usernameChangeHandler} />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" onChange={ageChangeHandler} />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    );
+};
 
 export default AddUser;
